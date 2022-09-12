4fc59ba861b9abdda1e75c7cbfdb2aefea91ac7f
ZoeyBatton <Hoodiescool@icloud.com>
2022-09-12 12:38:03 -0400

    Commit

---
+++ week_4/Bank_BalancePR.py
@@ -0,0 +1,11 @@
+customer_name = input("Welcome, what is your name?:")
+starting_balance = 5000.25
+
+print(f"Hello {customer_name}, your starting balance is ${starting_balance}.")
+
+pay_check = int(input("How much of you paycheck would you like to deposit? $"))
+
+expenditure_item = input("Looks like you went shopping. What did you buy? ")
+expenditure = int(input(f"How much does {expenditure_item} cost? $"))
+
+def checking_balance(user_name, balance, deposits, expense_item, expense_amount)
