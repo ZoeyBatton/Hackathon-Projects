f66a7d3796204ccb0c70acf54b3a163cfba72c81
Zoey Batton <Hoodiescool@icloud.com>
2022-09-22 13:07:45 -0400

    lists

---
+++ week_4/test.py
@@ -0,0 +1,7 @@
+one = float(input("What is your first number?: "))
+two = float(input("What is your second number?: "))
+
+def myCalcAdd(number1, number2 ):
+    return number1 + number2
+
+print(myCalcAdd(one, two))
